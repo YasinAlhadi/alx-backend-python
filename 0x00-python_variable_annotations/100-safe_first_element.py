7d1e2ed2a8edc5f64f2c1f030fbaf232619ff265
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-04-27 15:12:23 +0200

    add type annotations to the function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+""" Module that takes a list input of any type and returns first element"""
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """"""
+    """ Return first element of list or None"""
     if lst:
         return lst[0]
     else:
